043e1870fabdc7fea58731e4dfb0df2cc12859cc
dearterryk <122012745+dearterryk@users.noreply.github.com>
2023-05-29 22:46:13 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2023;
+        
+        answer = year - age;
+        
+        return answer;
+    }
+}
