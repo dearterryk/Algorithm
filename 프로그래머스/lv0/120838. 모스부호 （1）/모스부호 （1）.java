06a0510e66453bba96085598e60d4fd59a3d52bc
dearterryk <122012745+dearterryk@users.noreply.github.com>
2023-06-17 01:28:41 +0900

    [level 0] Title: 모스부호 (1), Time: 17.14 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] words = letter.split(" ");
+        
+        for(String word : words){
+            for(int i = 0; i<morse.length; i++){
+                if(morse[i].equals(word)){
+                    answer += (char)('a'+i);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
